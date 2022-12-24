2548799d05116835fb9744a02c78718095f37406
gem00n <o.ghanaiem@student.aaup.edu>
2022-12-25 00:36:38 +0200

    304. Storing & Fetching Data with SQLite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -18,10 +18,22 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace); // we added to the _items the new place
     notifyListeners();
-    DBHelper.insert('places', {
+    DBHelper.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
       'image': newPlace.image.path
     });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map((item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            loaction: null))
+        .toList();
+    notifyListeners();
   }
 }

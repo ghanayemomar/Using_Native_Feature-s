1a5446e74df3428849feb3869e486b55b2704111
gem00n <o.ghanaiem@student.aaup.edu>
2022-12-25 17:27:13 +0200

    309. Allowing Users to Pick a Location on the Map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+
 import '../helpers/location_helper.dart';
 import '../screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
   @override
-  State<LocationInput> createState() => _LocationInputState();
+  _LocationInputState createState() => _LocationInputState();
 }
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
+
   Future<void> _getCurrentUserLocation() async {
     final locData = await Location().getLocation();
-    final staicMapImageUrl = LocationHepler.generatedLocationPreviewImage(
+    final staticMapImageUrl = LocationHepler.generatedLocationPreviewImage(
       latitude: locData.latitude,
       longitude: locData.longitude,
     );
     setState(() {
-      _previewImageUrl = staicMapImageUrl;
+      _previewImageUrl = staticMapImageUrl;
     });
   }
 
   Future<void> _selectOnMap() async {
-    await Navigator.of(context).push(
+    final selectedLocation = await Navigator.of(context).push<LatLng>(
       MaterialPageRoute(
+        fullscreenDialog: true,
         builder: (ctx) => MapScreen(
           isSelecting: true,
         ),
       ),
     );
+    if (selectedLocation == null) {
+      return;
+    }
+    print(selectedLocation.latitude);
+    // ...
   }
 
   @override
@@ -39,11 +48,12 @@ class _LocationInputState extends State<LocationInput> {
           height: 170,
           width: double.infinity,
           alignment: Alignment.center,
-          decoration:
-              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
           child: _previewImageUrl == null
               ? Text(
-                  "No Location Picked",
+                  'No Location Chosen',
                   textAlign: TextAlign.center,
                 )
               : Image.network(
@@ -56,21 +66,23 @@ class _LocationInputState extends State<LocationInput> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextButton.icon(
-              label: Text(
-                "Current Location",
+              icon: Icon(
+                Icons.location_on,
               ),
+              label: Text('Current Location'),
+              // textColor: Theme.of(context).primaryColor,
               onPressed: _getCurrentUserLocation,
-              icon: Icon(Icons.location_on),
             ),
             TextButton.icon(
-              label: Text(
-                "Select On Map",
+              icon: Icon(
+                Icons.map,
               ),
+              label: Text('Select on Map'),
+              // textColor: Theme.of(context).primaryColor,
               onPressed: _selectOnMap,
-              icon: Icon(Icons.map),
-            )
+            ),
           ],
-        )
+        ),
       ],
     );
   }
